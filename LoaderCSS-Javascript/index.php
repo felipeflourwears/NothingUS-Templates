29c20a4cf4d0ca9306ca03124ec09a77c201003d
felipeflourwears <A01274880@itesm.mx>
2023-01-30 14:30:38 -0600

    Centrar mejor los divs del collague

---
+++ LoaderCSS-Javascript/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<h2>How To Create A Loader</h2>
+<div id="contenedor_carga">
+    <div id="loader"></div>
+</div>
+
+<section id="principal">
+<img src="leon.jpg" alt="">
+</section>
+
+
+
+<script src="script.js"></script>
+</body>
+</html>
