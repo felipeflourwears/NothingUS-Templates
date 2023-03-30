384db235877bd76dddeae8848d780bc941ee9813
felipeflourwears <A01274880@itesm.mx>
2023-03-30 09:31:04 -0600

    Upload multiple archivos

--- files_upload/files.php
+++ files_upload/files.php
@@ -1,4 +1,9 @@
 <?php
+//Variables de prueba
+$id_cliente2='2';
+$nombre_producto="producto";
+$descripcion="descripcion";
+
 
 $files_post = $_FILES['file'];
 
@@ -14,12 +19,19 @@ for ($i=0; $i < $file_count; $i++)
 	}
 }
 
+$fecha_actual = getdate();
+$fecha_actual_completa = $fecha_actual['mday'] . "-" . $fecha_actual['mon'] . "-" . $fecha_actual['year'] . $fecha_actual['hours'] . "-" . $fecha_actual['minutes'] . "-" . $fecha_actual['seconds'];
 
+	
 foreach ($files as $fileID => $file)
 {
-	$fileContent = file_get_contents($file['tmp_name']);
+    $nombre_imagen = $file['name'];
+    $temporal = $file['tmp_name'];
+    $carpeta = './imgprueba';
 
-	file_put_contents($file['name'], $fileContent);
+    $ruta = $carpeta . "/" . $id_cliente2 . "-" . $fecha_actual_completa . $nombre_imagen;
+	move_uploaded_file($temporal, $ruta);
+
 }
 
 
