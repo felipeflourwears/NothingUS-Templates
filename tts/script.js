6ffe72fa1069e78b9cc5a937202a97a76bc6a6f5
felipeflourwears <A01274880@tec.mx>
2023-06-15 12:47:51 -0600

    TTS Espanol

--- tts/script.js
+++ tts/script.js
@@ -7,25 +7,61 @@ isSpeaking = true;
 
 voices();
 
-function voices(){
-    for(let voice of synth.getVoices()){
-        let selected = voice.name === "Google US English" ? "selected" : "";
-        let option = `<option value="${voice.name}" ${selected}>${voice.name} (${voice.lang})</option>`;
-        voiceList.insertAdjacentHTML("beforeend", option);
+/* function voices() {
+    let selectedVoice = null; // Variable para almacenar la voz seleccionada
+  
+    for (let voice of synth.getVoices()) {
+      if (voice.name === "Google Español") {
+        selectedVoice = voice; // Guardar la voz seleccionada en la variable
+        break; // Salir del bucle una vez que se haya encontrado la voz deseada
+      }
     }
-}
+  
+    if (selectedVoice !== null) {
+      synth.voice = selectedVoice; // Establecer la voz seleccionada en el sintetizador
+      console.log(`Voz seleccionada: ${selectedVoice.name} (${selectedVoice.lang})`);
+    } else {
+      console.log("La voz deseada no está disponible.");
+    }
+  
+    return selectedVoice; // Devolver la voz seleccionada
+  } */
 
+  function voices() {
+    let selectedVoice = null; // Variable para almacenar la voz seleccionada
+  
+    for (let voice of synth.getVoices()) {
+      if (voice.lang.includes("es")) {
+        selectedVoice = voice; // Guardar la voz seleccionada en la variable
+        break; // Salir del bucle una vez que se haya encontrado una voz en español
+      }
+    }
+  
+    if (selectedVoice !== null) {
+      synth.voice = selectedVoice; // Establecer la voz seleccionada en el sintetizador
+      console.log(`Voz seleccionada: ${selectedVoice.name} (${selectedVoice.lang})`);
+    } else {
+      console.log("No se encontró una voz en español disponible.");
+    }
+  
+    return selectedVoice; // Devolver la voz seleccionada
+  }
+  
 synth.addEventListener("voiceschanged", voices);
 
-function textToSpeech(text){
+function textToSpeech(text) {
     let utterance = new SpeechSynthesisUtterance(text);
-    for(let voice of synth.getVoices()){
-        if(voice.name === voiceList.value){
-            utterance.voice = voice;
-        }
+    let selectedVoice = voices(); // Obtener la voz seleccionada
+  
+    if (selectedVoice !== null) {
+      utterance.voice = selectedVoice; // Establecer la voz seleccionada en el objeto Utterance
+    } else {
+      console.log("No se encontró una voz en español disponible.");
+      return;
     }
+  
     synth.speak(utterance);
-}
+  }
 
 speechBtn.addEventListener("click", e =>{
     e.preventDefault();
